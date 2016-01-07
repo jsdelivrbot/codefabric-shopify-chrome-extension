ad89cf4228e12a0e9ef84742ea4cfc09c21e7195
David Else <david@codefabric.co.uk>
2016-01-07 15:27:31 +0000

    Adding some logging

--- scripts/shopify-extension.js
+++ scripts/shopify-extension.js
@@ -10,6 +10,15 @@
         // Private
         var self = this;
 
+        //Shopify functions
+        var showMessage = function (message, isError) {
+          isError ? shopify.Flash.error(message) : shopify.Flash.notice(message);
+        };
+
+        var showError = function (error) {
+          showMessage(error, true);
+        };
+
         // HTML snippets
         var cardHtml = '<div class="next-card"></div>';
         var cardHeader = '<header class="next-card__header"></header>';
@@ -61,20 +70,15 @@
         };
 
         var loadProductExtensions = function(productId) {
-          var tabsCard = jq(cardHtml).addClass('tabs');
+          var tabsCard = jq(cardHtml).addClass('tabs-editor');
 
           addCardHeader.call(tabsCard, 'Tabs', [ 
             { handle: 'add-tab', title: 'Add a new tab', onClick: function() {} },
             { handle: 'tab-order', title: 'Change tab order', onClick: function() {} } 
           ]);
-          // var cardHeader =  tabsCard.find('header').append('<div class="next-grid next-grid--inner-grid next-grid--no-padding next-grid--vertically-centered"></div>').find('.next-grid');
-          // var cardContent = tabsCard.find('.next-card__section');
 
-          // cardHeader.append('<div class="next-grid__cell"><h2 class="next-heading">Tabs</h2></div>');
-          // var cardActions = cardHeader.append('<div class="next-grid__cell next-grid__cell--no-flex actions"><div class="next-grid next-grid--no-outside-padding next-grid--vertically-centered"></div></div>').find('.next-grid__cell.actions .next-grid');
-          
-          // var addTabAction = cardActions.append('<div class="next-grid__cell next-grid__cell--no-flex"><a class="action-add-tab" href="#">Add tab</a></div>').find('.action-add-tab');
-          // var tabOrderAction = cardActions.append('<div class="next-grid__cell next-grid__cell--no-flex"><a class="action-tab-order" href="#">Tab order</a></div>').find('.action-tab-order');
+
+
 
           jq('.next-card.images').before(tabsCard);
         };
@@ -84,7 +88,7 @@
           init: function () {
             debugger;
             if (!isShopify()) {
-              alert('Cannot be used outside of Shopify Admin!');
+              showError('CodeFabric Shopify extensions cannot be used outside of Shopify Admin!');
               return;
             }
 
@@ -106,6 +110,8 @@
                   break;
               }
             }
+
+            showMessage('CodeFabric Shopify extensions loaded!');
           }
         };
 
