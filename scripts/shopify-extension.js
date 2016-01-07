338ae33885421918b49281a341e0b2e8a95b36f1
David Else <david@codefabric.co.uk>
2016-01-07 12:02:30 +0000

    Adding checks for admin pages

--- scripts/shopify-extension.js
+++ scripts/shopify-extension.js
@@ -1,3 +1,133 @@
 (function() {
-  alert('Loaded!');
+  
+  var extensionJs = (function(shopify, jq) {
+    var CodeFabric = CodeFabric || {};
+    CodeFabric.Shopify = CodeFabric.Shopify || {};
+    if (!CodeFabric.Shopify.Extension) {
+
+      CodeFabric.Shopify.Extension = function () {
+
+        // Private
+        var self = this;
+
+        function isShopify = function () {
+          return /^http[s]?\:\\\\[^\\\/]+\.myshopify\.com\/admin\/.*$/i.test(location.href);
+        };
+
+        function getAdminPage = function () {
+          return location.href.match(/^http[s]?\:\\\\[^\\\/]+\.myshopify\.com\/admin\/([^\\\/]+)[\/]?(\d+)*.*$/i);
+        };
+
+        // Public
+        return {
+          init: function () {
+            if (!isShopify()) {
+              alert('Cannot be used outside of Shopify Admin!');
+              return;
+            }
+
+            var adminPage = getAdminPage();
+            jq('body').prepend('<p>' + JSON.stringify(adminPage) + '</p>');
+          }
+        };
+
+      };
+    }
+
+    var extension = new CodeFabric.Shopify.Extension();
+    extension.init();
+  });
+
+  /** Utilities and loading **/
+
+  var utils = {
+    loadScript: function (url, callback) {
+
+        var script = document.createElement("script")
+        script.type = "text/javascript";
+
+        if (script.readyState) {  //IE
+            script.onreadystatechange = function () {
+                if (script.readyState == "loaded" ||
+                        script.readyState == "complete") {
+                    script.onreadystatechange = null;
+                    callback();
+                }
+            };
+        } else {  //Others
+            script.onload = function () {
+                callback();
+            };
+        }
+
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);
+    },
+
+    loadCss: function (url, callback) {
+
+        var link = document.createElement("link")
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        link.media = "all";
+
+        if (link.readyState) {  //IE
+            link.onreadystatechange = function () {
+                if (link.readyState == "loaded" ||
+                        link.readyState == "complete") {
+                    link.onreadystatechange = null;
+                    callback();
+                }
+            };
+        } else {  //Others
+            link.onload = function () {
+                callback();
+            };
+        }
+
+        link.href = url;
+        document.getElementsByTagName("head")[0].appendChild(link);
+    },
+
+    ensureCss: function (callback) {
+      utils.loadCss('https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css', callback);
+    },
+
+    ensureJQuery: function (minVersion, src, callback) {
+      if ((typeof jQuery === 'undefined') || (parseFloat(jQuery.fn.jquery) < minVersion)) {
+        utils.loadScript(src, function () {
+            jqCodeFabric = jQuery.noConflict(true);
+            jqCodeFabric(function() { callback(jqCodeFabric); });
+        });
+      } else {
+        jQuery(function () { callback(jQuery); });
+      }
+    },
+
+    ensureShopify: function (callback) {
+      if (typeof Shopify === 'undefined') {
+        console.error('Not loaded from a Shopify store!')
+      } else {
+        callback(Shopify);
+      }
+    },
+
+    ensureDependencies: function (callback) {
+      utils.ensureCss(function() {
+        utils.ensureJQuery(1.9, '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+          function ($) {
+            utils.ensureShopify(function(shopify) {
+              callback(shopify, $);
+            });
+          }
+        );
+      });
+    }
+  };
+
+  utils.ensureDependencies(function (s, jq) {
+    extensionJs.call(this, s, jq, {});
+  });
+
+})();
 })();
