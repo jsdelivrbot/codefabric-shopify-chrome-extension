23cdd031c735a97e0dac09e01c7f1dd17f46c855
David Else <david@codefabric.co.uk>
2016-02-02 11:24:07 +0000

    Monkey-patch history state handling to raise window events

--- scripts/shopify-extension.js
+++ scripts/shopify-extension.js
@@ -965,6 +965,14 @@
       console.log(e);
       debugger;
     });
+    jq(window).on('pushstate', function(e) { 
+      console.log(e);
+      debugger;
+    });
+    jq(window).on('replacestate', function(e) { 
+      console.log(e);
+      debugger;
+    });
 
     jq(document).ready(function () {
       console.log('document ready called');
@@ -979,6 +987,19 @@
       console.log('before unload...');
       window.setTimeout(function() { extensionJs.call(self, s, jq, {}); }, 2000);
     });
+
+    if (window.history) {
+      var pushState = window.history.pushState;
+      window.history.pushState = function(state) {
+          jq(window).trigger('pushstate', state);
+          return pushState.apply(window.history, arguments);
+        };
+      var replaceState = window.history.replaceState;
+      window.history.replaceState = function(state) {
+          jq(window).trigger('replacestate', state);
+          return replaceState.apply(window.history, arguments);
+        };
+    }
   });
 
 })();
