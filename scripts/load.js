820579a024dfce2c4689f2270f9ec6e488aa194b
David Else <sum1youno@hotmail.com>
2017-01-09 20:03:25 +0000

    Updating the git commit in the built file too

--- scripts/load.js
+++ scripts/load.js
@@ -2,9 +2,9 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   /* 
   No tabs or host permissions needed!
   */
-  var branch = "99f0fd3b0d5842b4b43f790ca86c268ec55f8ea4";
+  var branch = "32f6d22cf8e6f1896f5a20bb2c26c2931e054603";
   var path = "https://rawgit.com/codefabric/codefabric-shopify-chrome-extension/" + (branch ? branch + "/" : "") + "scripts/";
-  path = "https://localhost:8080/scripts/";
+  //path = "https://localhost:8080/scripts/";
   var script = "shopify-extension.js";
   chrome.tabs.executeScript({
     code: 'var codefabric = document.createElement("script");codefabric.type = "text/javascript";codefabric.src = "' + path + script + '";document.getElementsByTagName("head")[0].appendChild(codefabric);'
